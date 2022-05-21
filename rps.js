4cbdcc67d7bc22580b8c058e512677f43cadbf2d
Horven <nrovrehs@yahoo.com>
2022-05-21 01:44:43 -0400

    Add game conditions

--- rps.js
+++ rps.js
@@ -1,17 +1,39 @@
-let playerSelection = prompt("Please choose your weapon.")
+let playerSelection =  prompt("Please choose your weapon.")
 
 function computerSelection() {
-    const outcomes = [/*"Rock", "Paper",*/ "Scissors"];
+    const outcomes = ["Rock", "Paper", "Scissors"];
 
     const random = Math.floor(Math.random() * outcomes.length);
     return outcomes[random]
 }
 
 function gameTime(playerSelection, computerSelection) {
-    if (playerSelection = computerSelection) {
-        console.log("Tie")
-    }
-        
-}
+    /*if (playerSelection = computerSelection) {
+        return "Tie."
+    }*/
+    if (playerSelection = "Rock") {
+        if (computerSelection = "Scissors") {
+            return "You win! Rock beats scissors!"
+        } else {
+                return "You lose. Paper beats rock."
+            }
+        }
+    
+    else if (playerSelection = "Paper") {
+        if (computerSelection = "Rock") {
+            return "You win! Paper beats rock!"
+        } else {
+                return "You lose. Scissor beats paper."
+            }
+        }
 
-console.log()
+    else if (playerSelection = "Scissors") {
+        if (computerSelection = "Paper") {
+            return "You win! Scissors beats paper!"
+        } else {
+                return "You lose. Rock beats paper."
+            }
+        }
+    }
+
+console.log(gameTime())
