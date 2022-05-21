dbb1c589c8191a72a7321f126836dc9c71276eda
Horven <nrovrehs@yahoo.com>
2022-05-21 02:19:26 -0400

    Debug game

--- rps.js
+++ rps.js
@@ -1,39 +1,26 @@
-let playerSelection =  prompt("Please choose your weapon.")
+let playerSelection = "Rock" /*prompt("Please choose your weapon.")*/
 
-function computerSelection() {
+function computerPlay() {
     const outcomes = ["Rock", "Paper", "Scissors"];
 
     const random = Math.floor(Math.random() * outcomes.length);
     return outcomes[random]
 }
 
-function gameTime(playerSelection, computerSelection) {
-    /*if (playerSelection = computerSelection) {
-        return "Tie."
-    }*/
-    if (playerSelection = "Rock") {
-        if (computerSelection = "Scissors") {
-            return "You win! Rock beats scissors!"
-        } else {
-                return "You lose. Paper beats rock."
-            }
-        }
-    
-    else if (playerSelection = "Paper") {
-        if (computerSelection = "Rock") {
-            return "You win! Paper beats rock!"
-        } else {
-                return "You lose. Scissor beats paper."
-            }
-        }
+const computerSelection = "Rock" /*computerPlay()*/;
 
-    else if (playerSelection = "Scissors") {
-        if (computerSelection = "Paper") {
-            return "You win! Scissors beats paper!"
-        } else {
-                return "You lose. Rock beats paper."
-            }
+function gameTime(playerSelection, computerSelection) {
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            return "Tie. Rocks."
+        } else if (computerSelection == "Paper") {
+            return "You lose. Paper beats rock."
+        } else if (computerSelection == "Scissors") {
+            return "You win! Rock beats Scissors!"
         }
     }
 
-console.log(gameTime())
+}
+
+console.log(playerSelection, computerSelection, gameTime(playerSelection, computerSelection))
+/*console.log(playerSelection == computerSelection)*/
