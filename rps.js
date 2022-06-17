5566361f224547640b7b89c1a2b487978cd5b877
Horven <nrovrehs@yahoo.com>
2022-06-17 01:16:52 -0400

    Complete game logic

--- rps.js
+++ rps.js
@@ -1,6 +1,12 @@
 const rock = document.getElementById("Rock")
 const paper = document.getElementById("Paper")
 const scissors = document.getElementById("Scissors")
+const buttons = document.querySelectorAll("button")
+
+var myScore = document.getElementById("myScore")
+var theirScore = document.getElementById("theirScore")
+var playerScore = 0
+var compScore = 0
 
 function computerPlay() {
     const outcomes = ["Rock", "Paper", "Scissors"];
@@ -15,8 +21,10 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "Rock") {
             return "Tie. Rocks."
         } else if (computerSelection == "Paper") {
+            compScore++
             return "You lose. Paper beats rock."
         } else if (computerSelection == "Scissors") {
+            playerScore++
             return "You win! Rock beats Scissors!"
         }
     }
@@ -24,8 +32,10 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "Paper") {
             return "Tie. Paper."
         } else if (computerSelection == "Scissors") {
+            compScore++
             return "You lose. Scissors beats paper."
         } else if (computerSelection == "Rock") {
+            playerScore++
             return "You win! Paper beats rock!"
         }
     }
@@ -33,8 +43,10 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "Scissors") {
             return "Tie. Scissors."
         } else if (computerSelection == "Rock") {
+            compScore++
             return "You lose. Rock beats Scissors."
         } else if (computerSelection == "Paper") {
+            playerScore++
             return "You win! Scissors beats paper!"
         }
     }
@@ -43,40 +55,33 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0
-    let compScore = 0
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please choose your weapon.")
-        computerSelection = computerPlay()
-        console.log(playRound(playerSelection, computerSelection))
-        if (playRound(playerSelection, computerSelection).includes("win")) {
-            playerScore++
-        } else if (playRound(playerSelection, computerSelection).includes("lose")) {
-            compScore++
-        }
-    }
-    console.log(compScore, playerScore)
-    document.getElementById("Score").innerHTML = playerScore
-}
-
-/*game()*/
-
 function fun1() {
     console.log("You have chosen rock.")
+    computerSelection = computerPlay()
     console.log(playRound("Rock", computerPlay()))
+    console.log(playerScore, compScore)
 }
 
 function fun2() {
     console.log("You have chosen paper.")
     console.log(playRound("Paper", computerPlay()))
+    console.log(playerScore, compScore)
 }
 
 function fun3() {
     console.log("You have chosen scissors.")
     console.log(playRound("Scissors", computerPlay()))
+    console.log(playerScore, compScore)
+}
+
+function update() {
+    myScore.innerHTML = playerScore
+    theirScore.innerHTML = compScore
 }
 
 rock.addEventListener("click", fun1)
 paper.addEventListener("click", fun2)
 scissors.addEventListener("click", fun3)
+buttons.forEach((button) => {
+    button.addEventListener("click", update)
+})
